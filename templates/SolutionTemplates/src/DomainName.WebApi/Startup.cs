c901f5f3c77c2c66a9966d61f619ede911159d82
Mladen <mladenristic1997@gmail.com>
2020-07-09 09:31:44 +0200

    web api raven registered explicitly, then added to serviceCollection

--- templates/SolutionTemplates/src/DomainName.WebApi/Startup.cs
+++ templates/SolutionTemplates/src/DomainName.WebApi/Startup.cs
@@ -17,13 +17,12 @@ namespace $safeprojectname$
 {
     public class Startup : ModularStartup
     {
-        public Startup(IConfiguration configuration) => Configuration = configuration;
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public new void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton(CreateRavenDbDocumentStore());
-            services.AddSingleton(Configuration);
+            var raven = CreateRavenDbDocumentStore();
+            services.AddSingleton(raven);
             services.AddTransient<ITimeProvider, TimeProvider>();
             services.AddTransient<ITypeaheadSmartSearchQuery, TypeaheadSmartSearchQuery>();
             services.AddTransient<IMessageBus, NSBus>();
